06ffe57ec9becb62309ad216f5a3a63fdab2fd78
Siddhi Sakharkar23 <130655566+Siddhisakharkar@users.noreply.github.com>
2024-05-30 09:25:18 +0530

    Add files via upload

---
+++ M1.java
@@ -0,0 +1,7 @@
+class M1 
+{
+  public static void main(String args[])
+  {
+    System.out.println("\n\ttwinkle twinkle little star \n\tHow  I wonder what you are \n\tup Above the whats so high \n\tLike a diamind in the sky");
+  }
+}
