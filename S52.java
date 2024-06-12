32699981a28aadaaf9e513c5819b59eec839b40b
Siddhi Sakharkar23 <130655566+Siddhisakharkar@users.noreply.github.com>
2024-06-12 10:54:05 +0530

    Add files via upload

---
+++ S52.java
@@ -0,0 +1,16 @@
+/*Write a Java program to display the first 10 natural numbers.*/
+
+import java.util.*;
+class S52
+{
+  public static void main(String args[])
+  {
+    int i ;
+    System.out.println("The first antural numbers are :");
+    for(i = 1; i <= 10; i++)
+    {
+      System.out.println(i);
+    }
+    System.out.println("\n");
+  }
+}
