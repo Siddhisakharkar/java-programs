b685574e5f7b57882629fd8f6cb79039f06eab8f
Siddhi Sakharkar23 <130655566+Siddhisakharkar@users.noreply.github.com>
2024-06-13 23:26:59 +0530

    Create S55.java

---
+++ S55.java
@@ -0,0 +1,16 @@
+/*write a java program to check whethed the person is applicabke for votong or not*/
+class Main 
+{
+public static void main(String args[])
+{
+int a = 18;
+if(a > 18 )
+{
+System.out.println("The person is eligible");
+}
+else
+{
+System.out.println("The person is not eligible");
+}
+}
+}
