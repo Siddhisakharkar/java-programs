10595c14a9f62548c6c8928c91aa22f11705b9bd
Siddhi Sakharkar23 <130655566+Siddhisakharkar@users.noreply.github.com>
2024-06-14 23:13:00 +0530

    Create S56.java

---
+++ S56.java
@@ -0,0 +1,18 @@
+/*write a java program to check the greater number*/
+import java.util.* ;
+xlass S56
+  {
+  public static void main(String args[])
+    {
+    int a = 2;
+    int b = 5;
+    if(a > b)
+    {
+      System.out.println("a is greater than b");
+    }
+    else
+    {
+      System.out.println("print the lowest number");
+    }
+  }
+}
